ef7a9456a4ebc3559565fcca590fb5ee7deeceeb
Alberto-1905 <148698281+Alberto-1905@users.noreply.github.com>
2023-10-22 22:35:57 -0600

    Add files via upload

---
+++ pelicula/pagina.php
@@ -0,0 +1,233 @@
+
+<?php require '../php/session.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Open+Sans:wght@400;600&display=swap" rel="stylesheet"> 
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+		<link rel="stylesheet" href="/pelicula/css/stylo.css">
+		<title>home</title>
+	</head>
+
+	<body>
+		<!-- Header -->
+		<main>			
+			<div class="content-all">
+				<header></header>
+				<label>
+					<p><a style="text-decoration: none; color:white" href="/pelicula/pagina.php">Películas</a></p>
+				</label>
+
+				<div class="btn">
+					<p><a href="/php/logout.php"><img src="/pelicula/img/off.png"></a></p>
+				</div>
+			</div>
+		</main>
+
+		<div class="container">
+
+			<div class="item">
+				<a href="peliculas/monja2.php"><img src="img/monja2.jpg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/lugarensilencio.php"><img src="img/lugarensilecio.jpg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/it2.php"><img src="img/it2.jpg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/horadetumuerte.php"><img src="img/horadetumuerte.jpg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/terrifier.php"><img src="img/terrifier.jpg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/elconjuro.php"><img src="img/elconjuro.jpeg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/cienpieshumano.php"><img src="img/cienpieshumano.jpg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/M3GAN.php"><img src="img/m3gan.jpg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/ruegapornosotros.php"><img src="img/ruegapornosotros.jpg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/x.php"><img src="img/x.jpg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/doctorstrange.php"><img src="img/doctorstrange.jpg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/avatar.php"><img src="img/avatar.jpg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/godzillaskong.php"><img src="img/godzillavskong.jpg" class="item-img"></a>
+			</div>
+			<!--7 Prisioneros-->
+			<div class="item">
+				<a href="peliculas/batman.php"><img src="img/batman.jpg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/jurassicworld.php"><img src="img/jurassicworld.jpg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/thor.php"><img src="img/thor.jpg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/rapidosyfuriososx.php"><img src="img/rapidosyfuriososx.jpeg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/transformers.php"><img src="img/transformers.jpg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/gigantesdeacero.php"><img src="img/gigantesdeacero.jpg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/blackwidow.php"><img src="img/blackwidow.jpg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/afterenmilpedazos.php"><img src="img/After.jpeg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/365dias.php"><img src="img/365dias.jpg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/yoantesdeti.php"><img src="img/yoantesdeti.jpg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/corazonesmalheridos.php"><img src="img/corazonesmalheridos.jpg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/ghosting.php"><img src="img/Ghosting.jpg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/titanic.php"><img src="img/titanic.jpg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/votosdeamor.php"><img src="img/votosdeamor.jpg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/lapropuesta.php"><img src="img/lapropuesta.jpg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/atravezdemiventana.php"><img src="img/atravesdemiventana.jpg" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/magicmike.php"><img src="img/magicmike.jpg" class="item-img"></a>
+			</div>
+<!-- aqui estoy -->
+			<!-- <div class="item">
+				<a href="peliculas/ejercito-de-los-ladrones.php"><img src="" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/ice-road.php"><img src="" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/el-conjuro-3.php"><img src="" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/ejercito-de-los-muertos.php"><img src="" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/el-mesero.php"><img src="" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/mi-primer-beso-3.php"><img src="" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/escape-room-2.php"><img src="" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/el-escuadron-suicida.php"><img src="" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/fauces-de-la-noche.php"><img src="" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/fuego-con-fuego.php"><img src="" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/free-guy.php"><img src="" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/godzilla-vs-kong.php"><img src="" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/halloween-kills.php"><img src="" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/hoy-si.php"><img src="" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/infinite.php"><img src="" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/inside-man.php"><img src="" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/jefe-en-pañales-2.php"><img src="" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/la-liga-de-la-justicia.php"><img src="" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/la-patrulla-canina.php"><img src="" class="item-img"></a>
+			</div>
+
+			<div class="item">
+				<a href="peliculas/la-purga-infinita.php"><img src="" class="item-img"></a>
+			</div> -->
+		</div>
+
+	</body>
+</html>
